4262373e8fd89f60a65e000d9bbf19f001c34a2c
utkallearninghub <utkallearninghub@gmail.com>
2026-02-01 19:16:08 +0530

    01-02-2026

--- src/components/order/Orders.tsx
+++ src/components/order/Orders.tsx
@@ -1,31 +1,59 @@
 import "./Orders.css";
-
-import product from "../../assets/product_2.png"
+import { useEffect, useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
+import type { OrderModel } from "../../models/OrderModel";
+
+import productImage from "../../assets/product_2.png"
 
 const Orders = () => {
-    return (
-        <div className="orders-panel">
-            <div className="order-card">
-                <div className="order-header">
-                    <div className="status">
-                        <span className="check"><FaAngleDown /></span>
-                        <div>
-                            <div>Arrived in 12 minutes</div>
-                            <div>₹300 · 20 Jan, 7:41 pm</div>
-                        </div>
-                    </div>
-                    <span className="arrow">→</span>
-                </div>
+  const [orders, setOrders] = useState<OrderModel[]>([]);
+  const [loading, setLoading] = useState(true);
 
-                <div className="order-items">
-                    <img src={product} />
-                    <img src={product} />
-                    <img src={product} />
+  useEffect(() => {
+    fetch("/mock/orders.json") // from public folder
+      .then((res) => res.json())
+      .then((data: OrderModel[]) => {
+        setOrders(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading orders...</p>;
+
+  return (
+    <div className="orders-panel">
+      {orders.map((order) => (
+        <div key={order.orderId} className="order-card">
+          <div className="order-header">
+            <div className="status">
+              <span className="check">
+                <FaAngleDown />
+              </span>
+
+              <div>
+                <div>Arrived in {order.deliveryTime}</div>
+                <div>
+                  ₹{order.totalAmount} · {order.date}
                 </div>
+              </div>
             </div>
+
+            <span className="arrow">→</span>
+          </div>
+
+          <div className="order-items">
+            {order.items.map((item) => (
+              <img key={item.id} src={productImage} alt={item.name} />
+            ))}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Orders;
