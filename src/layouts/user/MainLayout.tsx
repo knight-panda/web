a69a97077e5722b679ca2610ea023d1568a39051
GSL <android@xyug.in>
2026-02-16 18:14:00 +0530

    16-02-2026

--- src/layouts/user/MainLayout.tsx
+++ src/layouts/user/MainLayout.tsx
@@ -1,6 +1,5 @@
 import Footer from "../../components/footer/Footer";
-import Navbar from "../../components/navbar/Navbar"
-import Home from "../../pages/user/home/Home"
+import Navbar from "../../components/navbar/Navbar";
 import { Outlet, useNavigate } from 'react-router-dom';
 import ScrollToTop from "../../utils/ScrollToTop";
 
@@ -10,6 +9,7 @@ const MainLayout = () => {
   const handleNavigation = (id: string) => {
     navigate(id);
   };
+
   return (
     <div>
       <ScrollToTop />
