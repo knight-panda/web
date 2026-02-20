65e04e72ac96b00364cb7b12445fef19ec4f9fbb
GSL <android@xyug.in>
2026-02-20 15:51:43 +0530

    20-02-2026

--- src/layouts/admin/AdminLayout.tsx
+++ src/layouts/admin/AdminLayout.tsx
@@ -3,14 +3,17 @@ import AdminDashboard from '../../pages/admin/dashboard/AdminDashboard'
 import AdminNavbar from '../../components/navbar/AdminNavbar'
 import AdminFooter from '../../components/footer/AdminFooter'
 import AdminHome from '../../pages/admin/home/AdminHome'
+import OurServices from '../../pages/admin/ourServices/OurServices'
+import AdminPricing from '../../pages/admin/pricing/AdminPricing'
 
 const AdminLayout = () => {
   return (
     <div>
-        <AdminNavbar />
-        <AdminHome />
-        <AdminDashboard/>
-        {/* <AdminFooter /> */}
+      <AdminNavbar />
+      <AdminHome />
+      <OurServices />
+      <AdminPricing />
+      <AdminFooter />
     </div>
   )
 }
